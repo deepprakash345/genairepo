65079804cedf46d40fc55b03be8f804153656664
Deep Prakash Dewanji <deepprakash345@gmail.com>
2026-04-17 14:41:29 +0530

    Update form-editor-support.js

--- scripts/form-editor-support.js
+++ scripts/form-editor-support.js
@@ -397,7 +397,7 @@ export function attachEventListners(main) {
 }
 
 function getAuthoringAssistantUrl() {
-  const LOCAL_URL = 'https://localhost.corp.adobe.com:8014/dist/universal_editor.html';
+  const LOCAL_URL = 'https://localhost.corp.adobe.com:8014/universal_editor.html';
   const PROD_URL = 'https://experience.adobe.com/solutions/livecycle-authoring-assistant-spa/static-assets/universal_editor.html';
   const STAGE_URL = 'https://experience-stage.adobe.com/solutions/livecycle-authoring-assistant-spa/static-assets/universal_editor.html';
 
